eb8ab2bc0cf7b1fef91d103cf3b53536b93a1058
Feichuan <Feichuan05@gamil.com>
2024-07-21 00:23:03 +0800

    新增了Person组件,导入并注册到了App.vue中 methods方法声明 有点像面对对象,data保存数据,methods声明放方法,使用this

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
     <div class="app">
         <h1>你好啊！</h1>
+        <Person/>
     </div>
 </template>
 
 <script lang="ts">
     //JS或TS
+    import Person from './compoments/Person.vue';//导入组件
     // 暴露组件名
     export default {
-        name: 'App' //组件名
+        name: 'App' ,//组件名
+        components:{Person} // 注册组件
     }
+    
 </script>
 
 <style>
---
+++ src/compoments/Person.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="person">
+        <h2>姓名:{{ name }}</h2>
+        <h2>年龄:{{ age}}</h2>
+        <button @click="changeName">修改名字</button>
+        <button @click="changeAge">修改年龄</button>
+        <button @click = "showTel">查看联系方式</button>
+    </div>
+</template>
+
+<script lang='ts'>
+import { log } from 'console';
+import { isNumberObject } from 'util/types';
+
+// import { tel } from 'vue';
+
+//JS或TS
+export default {
+        name:'Person',
+        data(){
+            return{
+                name:'张三',
+                age:18,
+                tel:"8888888888",
+            }
+        },
+        methods: {
+            showTel(){
+                alert(this.tel)
+            },
+            changeName(){
+                this.name = prompt("请输入明治")
+            },
+            changeAge(){
+                var num = prompt("请输入数字")
+                this.age = num
+            },
+        }
+    }
+</script>
+
+<style>
+/*  样式  */
+
+.person{
+    background-color: skyblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+button{
+    margin: 0 5px;
+}
+</style>
