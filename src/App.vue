031277584be3d268a47111c192a12461ed898f2a
Feichuan <Feichuan05@gamil.com>
2024-07-26 18:39:00 +0800

    标签的ref属性

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
-        <Person/>
+        <h2 ref="title2">2222</h2>
+        <button @click="showLog">点我输出h2</button>
+        <Person ref = "ren"/>
+        <Person ref = "ren2"/>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup name="App">
     //JS或TS
     import Person from './compoments/Person.vue';//导入组件
-    // 暴露组件名
-    export default {
-        name: 'App' ,//组件名
-        components:{Person} // 注册组件
+    import {ref} from 'vue';
+    let title2 = ref()
+    let ren = ref()
+    function showLog(){
+        console.log(title2.value)
+        console.log(ren.value.title2);
+        
     }
-    
 </script>
+
 
--- src/compoments/Person.vue
+++ src/compoments/Person.vue
@@ -1,48 +1,25 @@
 <template>
     <div class="person">
-        <h2>当水温达到 60°,或水位达到 80cm 时，给服务器发请求</h2>
-        <h2>当前水温:{{ temp }}</h2>
-        <h2>当前水位:{{ height }}</h2>
-        <button @click="changeTemp">水温 +1</button>
-        <button @click="changeHeight">点我 +1</button>
+        <h1>China</h1>
+        <h2 ref="title2">Beijing</h2>
+        <h3>尚硅谷</h3>
+        <button @click="showLog">点我输出 h2 这个袁术</button>
     </div>
 
 </template>
 
 <script lang="ts" setup name='Person'>
-    import { log } from 'console';
-import { ref, watchEffect } from 'vue';
-
-    var temp = ref(10);
-    var height = ref(0);
-
-    function changeTemp() {
-        temp.value += 10;
-    }
-    function changeHeight() {
-        height.value += 10;
-    }
-    // watch 写法
-    // let tempWatch = watch(temp,()=>{
-    //     if(temp.value>=60){
-    //         alert('dddd')
-    //         tempWatch()
-    //     }
-    // })
-const x = watchEffect(()=>{
-    if(temp.value>0){
-        console.log("222");
+    import {ref} from 'vue';
+    //  创建一个title2,用于存储ref标记的内容
+    let title2 = ref()
+    function showLog(){
+        console.log(title2.value);
         
     }
-    if(temp.value>99){
-        console.log("结束了");
-        
-        x()
-    }
-})
+    defineExpose({title2})
 </script>
 
-<style>
+<style scoped>
 /*  样式  */
 
 .person {
