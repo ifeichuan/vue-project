4d6fd425047821075524dfa1d0baaff54c67f28b
Feichuan <Feichuan05@gamil.com>
2024-07-26 16:07:00 +0800

    watch监视_0405

--- src/compoments/Person.vue
+++ src/compoments/Person.vue
@@ -1,36 +1,53 @@
 <template>
     <div class="person">
-        <h1>情况二:监视[ref] 定义对象类型</h1>
-        <h2>姓名:{{ person.name }}</h2>
-        <h2>年龄:{{ person.age }}</h2>
-        <button @click="changeName">修改名字</button>
+        <h2>{{ person.name }}</h2>
+        <h2>{{ person.age}}</h2>
+        <h2>{{ person.car.c1 }},{{ person.car.c2 }}</h2>
+        <button @click="changeName">修改明治</button>
         <button @click="changeAge">修改年龄</button>
-        <button @click="changePerson">修改整个人</button>
+        <button @click="changeC1">修改第一台车</button>
+        <button @click="changeC2">修改第二台车</button>
+        <button @click="changeCar">修改整台车</button>
     </div>
 
 </template>
 
 <script lang="ts" setup name = 'Person'>
-    import { reactive,watch} from 'vue';
-    // 数据
+    import { reactive,watch } from 'vue';
     let person = reactive({
         name:'张三',
-         age:18
-        })
-    // 方法
+        age:18,
+        car:{
+            c1:'奔驰',
+            c2:'宝马'
+        }
+    })
     function changeName(){
         person.name += '~'
+        // Object.assign(person,{name:'lis'})
     }
     function changeAge(){
-        person.age +=1
+        person.age += 1
     }
-    function changePerson(){
-        //  批量修改,地址不更改
-        Object.assign(person,{name:'李四',age:20})
+    function changeC1(){
+        person.car.c1 = '奥迪'
     }
-    watch(person,(newValue,oldValue)=>{
-        console.log(person,newValue,oldValue)
-    })
+    function changeC2(){
+        person.car.c2 = 'AE86'
+    }
+    function changeCar(){
+        person.car = {c1:'雅迪',c2:'帝豪'}
+    }
+    // 第四种情况，监视对象中某个值，可以使用函数式返回该值
+    // watch(()=>person.car,(newValue,oldValue)=>{
+    //     console.log(newValue,oldValue);
+        
+    // },{deep:true})
+    // 第五种情况
+    watch([()=>person.car,()=>person.name],(newValue,oldValue)=>{
+        console.log(newValue,oldValue);
+        
+    },{deep:true})
 </script>
 
 <style>
