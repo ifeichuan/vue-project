3f625f5e38d3d454b8e49b9855b909f303fd2b39
Feichuan <Feichuan05@gamil.com>
2024-07-30 00:41:15 +0800

    latest1

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
     import Person from './compoments/Person.vue';//导入组件
     // import {Persons} from '@/types';
     import {ref} from 'vue';
-    let personList:Persons= [
+    let personList= [
     {id:'121312x',name:'张三',age:60},
     {id:'1213122x',name:'张三',age:61},
     {id:'121312222x',name:'张三',age:30}
